170c56f84cf2b4ad54c9cb9ebb66b277edfdc2ae
rizvan <rizvangaripov@gmail.com>
2023-11-29 18:39:59 +0300

    add non working course list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:fruc_education/request.dart';
+
+import 'courses.dart';
 
 void main() {
   runApp(const MyApp());
@@ -56,6 +61,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  late Future<List<Course>> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -76,50 +88,28 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return FutureBuilder<List<Course>>(
+      future: futureAlbum,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          MaterialApp(
+              home:  Scaffold(
+                body: ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: snapshot.data?.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Text(snapshot.data![index].name, style: const TextStyle(fontSize: 22));
+                    }
+                ),
+                appBar: AppBar(title: Text("METANIT.COM")),)
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        // By default, show a loading spinner.
+        return const CircularProgressIndicator();
+      },
     );
   }
 }
---
+++ lib/request.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+import 'courses.dart';
+
+Future<List<Course>> fetchAlbum() async {
+  final response = await http.get(
+    Uri.parse('https://apidev.baze.pro/v1/course?limit=999&page=0'),
+    headers: {
+      HttpHeaders.authorizationHeader: 'APIKEY mh5PhBx4W19uqjfgNvQvRslDelnAVLLdr6vpCyrkvfxbbcAItMPPfpkghgRT0yufR92CvwXM35XOPcMU5Gc4Ud2eaO6fIwSCBgAREheuKPjMvimd7vzIYUkbfVH8EAOglFXff9jWPo7Z5PF3ao4FRLBXw3pGuXNY2srz7YJeWmeWjq7gOT4Km2hsqO9Kle1HoVrOF6K5qvjTM6EjX40Z98QEbVegVejgk90FgJI',
+    },
+  );
+
+  if (response.statusCode == 200) {
+    // If the server returned a 200 OK response, parse the JSON.
+    final dynamic jsonResponse = jsonDecode(utf8.decode(response.bodyBytes));
+    if (jsonResponse is Map<String, dynamic> &&
+        jsonResponse.containsKey('content') &&
+        jsonResponse['content'] is List) {
+      final List<dynamic> coursesJson = jsonResponse['content'];
+      return coursesJson.map((courseJson) => Course.fromJson(courseJson)).toList();
+    } else {
+      throw Exception('Invalid or unexpected JSON format');
+    }
+  } else {
+    // If the server did not return a 200 OK response, throw an exception.
+    throw Exception('Failed to load courses');
+  }
+}
